7ea5a9a1bf8b3cb0decfa351c24108428f2506e9
Karthick <karthickraja3018@gmail.com>
2022-06-08 22:26:30 +0530

    cleaning folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { Suspense } from "react";
-import Batching from "./batching";
-import SuspenseComponent from "./suspense";
-import TransitionButton from './transition';
-
 function App() {
-  const Loader = () => {
-    console.log('here')
-    return (
-      <div>
-        <h1>fetching pokemons ......</h1>
-      </div>
-    );
-  }
   return (
     <div className="App">
       <h1>react 18</h1>
-      <Batching />
-      <Suspense fallback={<Loader />}>
-        <SuspenseComponent />
-      </Suspense>
-      <TransitionButton />
     </div>
   );
 }
